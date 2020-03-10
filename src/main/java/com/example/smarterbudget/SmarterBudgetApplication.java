4cd53bef03c27dc0ef30ff7e32455016adc3da82
JFocht96 <jared_focht@yahoo.com>
2020-03-09 20:55:48 -0400

    Initial commit

---
+++ src/main/java/com/example/smarterbudget/SmarterBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.example.smarterbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarterBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarterBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smarterbudget/SmarterBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smarterbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarterBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
